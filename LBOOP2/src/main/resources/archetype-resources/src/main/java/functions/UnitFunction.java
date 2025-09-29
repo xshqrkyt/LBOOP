e68f1a4f52866f869cea68bd3b7f283d1b7adcfc
Me-Di-A <melnicov730@gmail.com>
2025-09-29 21:33:42 +0300

    Add ConstantFunction.java, UnitFunction.java and Zero.Function.java

---
+++ LBOOP2/src/main/resources/archetype-resources/src/main/java/functions/UnitFunction.java
@@ -0,0 +1,7 @@
+package $ru.ssau.tk._NAME_._PROJECT_.functions;
+
+public class UnitFunction extends ConstantFunction {
+    public UnitFunction() {
+        super(1);
+    }
+}
---
+++ LBOOP2/src/main/resources/archetype-resources/src/main/java/functions/ZeroFunction.java
@@ -0,0 +1,7 @@
+package $ru.ssau.tk._NAME_._PROJECT_.functions;
+
+public class ZeroFunction extends ConstantFunction {
+    public ZeroFunction () {
+        super(0);
+    }
+}
