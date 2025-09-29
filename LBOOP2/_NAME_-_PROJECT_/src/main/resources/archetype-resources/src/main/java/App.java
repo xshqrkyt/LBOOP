6e431efc5a00633aee72157544f03c7dff61b5de
xShqrk <135094833+xshqrkyt@users.noreply.github.com>
2025-09-29 21:53:50 +0400

    create lboop2 dir

---
+++ LBOOP2/_NAME_-_PROJECT_/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $ru.ssau.tk._NAME_._PROJECT_;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
