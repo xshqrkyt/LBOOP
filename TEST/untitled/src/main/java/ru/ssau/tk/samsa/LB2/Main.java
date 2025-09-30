41930eb31d5d371910d78292d67e99bb2d94ced0
xShqrk <135094833+xshqrkyt@users.noreply.github.com>
2025-09-30 16:12:10 +0400

    Add files via upload

---
+++ TEST/untitled/src/main/java/ru/ssau/tk/samsa/LB2/Main.java
@@ -0,0 +1,7 @@
+package ru.ssau.tk.samsa.LB2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
---
+++ TEST/untitled/src/main/java/ru/ssau/tk/samsa/LB2/functions/ZeroFunction.java
@@ -0,0 +1,7 @@
+package ru.ssau.tk.samsa.LB2.functions;
+
+public class ZeroFunction extends ConstantFunction {
+    public ZeroFunction () {
+        super(0);
+    }
+}
