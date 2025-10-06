b685095227310dbd59ba41c4a9dc0c2e699558c3
Me-Di-A <melnicov730@gmail.com>
2025-10-06 23:05:11 +0300

    Update FunctionsIO.java

--- LBOOP3/untitled/src/main/java/ru/ssau/tk/samsa/LB2/io/FunctionsIO.java
+++ LBOOP3/untitled/src/main/java/ru/ssau/tk/samsa/LB2/io/FunctionsIO.java
@@ -1,7 +1,7 @@
 package ru.ssau.tk.samsa.LB2.io;
 
-import ru.ssau.tk.samsa.LB2.functions.TabulatedFunction;
-import ru.ssau.tk.samsa.LB2.functions.Point;
+import ru.ssau.tk.samsa.LB2.functions.*;
+import ru.ssau.tk.samsa.LB2.functions.factory.*;
 
 import java.io.*;
 import java.text.NumberFormat;
@@ -10,9 +10,43 @@ import java.util.Locale;
 
 public final class FunctionsIO {
     private FunctionsIO() {
-        throw new UnsupportedOperationException("Utility class cannot be instantiated");
+        throw new UnsupportedOperationException("This constructor has not been implemented yet.");
     }
 
+    public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) {
+        PrintWriter printWriter = new PrintWriter(writer);
+        printWriter.println(function.getCount());
+
+        Iterator<Point> iterator = function.iterator();
+        for (Point point : function)
+            printWriter.printf("%f %f\n", point.x, point.y);
+
+        printWriter.flush();
+    }
+
+    public static TabulatedFunction readTabulatedFunction(BufferedReader reader, TabulatedFunctionFactory factory) throws IOException {
+        int count = Integer.parseInt(reader.readLine());
+        double[] xValues = new double[count];
+        double[] yValues = new double[count];
+
+        NumberFormat format = NumberFormat.getInstance(Locale.forLanguageTag("ru"));
+
+        for (int i = 0; i < count; ++i) {
+            String[] str = reader.readLine().split(" ");
+
+            try {
+                xValues[i] = format.parse(str[0]).doubleValue();
+                yValues[i] = format.parse(str[1]).doubleValue();
+            }
+
+            catch (ParseException error) {
+                throw new IOException(error);
+            }
+        }
+
+        return factory.create(xValues, yValues);
+    }
+    
     public static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function) throws IOException {
         DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
         dataOutputStream.writeInt(function.getCount());
@@ -24,6 +58,7 @@ public final class FunctionsIO {
 
         dataOutputStream.flush();
     }
+    
     public static TabulatedFunction readTabulatedFunction(BufferedInputStream inputStream, TabulatedFunctionFactory factory) throws IOException {
         DataInputStream dataInputStream = new DataInputStream(inputStream);
         int count = dataInputStream.readInt();
@@ -37,6 +72,7 @@ public final class FunctionsIO {
 
         return factory.create(xValues, yValues);
     }
+    
     public static void serialize(BufferedOutputStream stream, TabulatedFunction function) throws IOException {
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(stream);
         objectOutputStream.writeObject(function);
