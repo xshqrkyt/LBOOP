c8d6f804f01711297c105980b4745b20295ce5cc
xShqrk <135094833+xshqrkyt@users.noreply.github.com>
2025-10-07 17:23:04 +0400

    Add files via upload

--- LBOOP3/untitled/src/main/java/ru/ssau/tk/samsa/LB2/operations/TabulatedDifferentialOperator.java
+++ LBOOP3/untitled/src/main/java/ru/ssau/tk/samsa/LB2/operations/TabulatedDifferentialOperator.java
@@ -2,8 +2,8 @@ package ru.ssau.tk.samsa.LB2.operations;
 
 import ru.ssau.tk.samsa.LB2.functions.Point;
 import ru.ssau.tk.samsa.LB2.functions.TabulatedFunction;
-import ru.ssau.tk.samsa.LB2.functions.factory.ArrayTabulatedFunctionFactory;
-import ru.ssau.tk.samsa.LB2.functions.factory.TabulatedFunctionFactory;
+import ru.ssau.tk.samsa.LB2.factory.ArrayTabulatedFunctionFactory;
+import ru.ssau.tk.samsa.LB2.factory.TabulatedFunctionFactory;
 
 public class TabulatedDifferentialOperator implements DifferentialOperator<TabulatedFunction> {
     private TabulatedFunctionFactory factory;
@@ -24,27 +24,40 @@ public class TabulatedDifferentialOperator implements DifferentialOperator<Tabul
         this.factory = factory;
     }
 
+
+
     @Override
     public TabulatedFunction derive(TabulatedFunction function) {
+        this.currentFunction = function;
+        this.currentDerivative = calculateDerivative(function);
+        return currentDerivative;
+    }
+
+    @Override
+    public double apply(double x) {
+        if (currentDerivative == null) {
+            throw new IllegalStateException("Сначала нужно вызвать derive()");
+        }
+        return currentDerivative.apply(x);
+    }
+
+    private TabulatedFunction calculateDerivative(TabulatedFunction function) {
+
         Point[] points = TabulatedFunctionOperationService.asPoints(function);
         int count = points.length;
         double[] xValues = new double[count];
         double[] yValues = new double[count];
 
-        // Копируем x значения
         for (int i = 0; i < count; i++) {
             xValues[i] = points[i].x;
         }
 
-        // Вычисляем производные (правая разность)
         for (int i = 0; i < count - 1; i++) {
             yValues[i] = (points[i + 1].y - points[i].y) / (points[i + 1].x - points[i].x);
         }
-
-        // Для последней точки используем левую разность
         yValues[count - 1] = yValues[count - 2];
 
         return factory.create(xValues, yValues);
     }
-
+}
 }
