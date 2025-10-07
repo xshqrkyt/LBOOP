9942356ef5a7ca0ffe822765ec2f69f10c04d82a
Me-Di-A <melnicov730@gmail.com>
2025-10-07 11:06:14 +0300

    Update AbstractTabulatedFunction.java

--- LBOOP3/untitled/src/main/java/ru/ssau/tk/samsa/LB2/functions/AbstractTabulatedFunction.java
+++ LBOOP3/untitled/src/main/java/ru/ssau/tk/samsa/LB2/functions/AbstractTabulatedFunction.java
@@ -1,5 +1,7 @@
 package ru.ssau.tk.samsa.LB2.functions;
 
+import ru.ssau.tk.samsa.LB2.exceptions.*;
+
 public abstract class AbstractTabulatedFunction implements MathFunction, TabulatedFunction {
     protected abstract int floorIndexOfX(double x);
     protected abstract double extrapolateLeft(double x);
@@ -8,6 +10,7 @@ public abstract class AbstractTabulatedFunction implements MathFunction, Tabulat
     protected double interpolate(double x, double leftX, double rightX, double leftY, double rightY) {
         return leftY + (rightY - leftY) / (rightX - leftX) * (x - leftX);
     }
+    
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
@@ -39,5 +42,20 @@ public abstract class AbstractTabulatedFunction implements MathFunction, Tabulat
                 return getY(indexOfX(x));
             else
                 return interpolate(x, floorIndexOfX(x));
+    }
+
+    static void checkLengthIsTheSame(double[] xValues, double[] yValues) {
+        if (xValues.length != yValues.length)
+            throw new DifferentLengthOfArraysException("The lengths of the arrays do not match.");
+    }
+
+    static void checkSorted(double[] xValues) {
+        int i = 0, n = xValues.length - 1;
+        while (i < n) {
+            if (xValues[i] >= xValues[i + 1])
+                throw new ArrayIsNotSortedException("The elements in the array are not ordered.");
+
+            ++i;
+        }
     }
 }
