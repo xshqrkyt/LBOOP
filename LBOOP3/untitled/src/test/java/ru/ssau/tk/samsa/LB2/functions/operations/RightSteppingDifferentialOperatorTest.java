eda54cb7d43f4058683e4865ab1880b407439753
Me-Di-A <melnicov730@gmail.com>
2025-10-11 19:41:48 +0300

    Update RightSteppingDifferentialOperatorTest.java

--- LBOOP3/untitled/src/test/java/ru/ssau/tk/samsa/LB2/functions/operations/RightSteppingDifferentialOperatorTest.java
+++ LBOOP3/untitled/src/test/java/ru/ssau/tk/samsa/LB2/functions/operations/RightSteppingDifferentialOperatorTest.java
@@ -1,14 +1,13 @@
 package ru.ssau.tk.samsa.LB2.operations;
 
-import ru.ssau.tk.samsa.LB2.functions.*;
-
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+
+import ru.ssau.tk.samsa.LB2.functions.*;
 
 class RightSteppingDifferentialOperatorTest {
     private static final double EPS = 1e-1;
     private static final double STEP = 1e-3;
-
     private final MathFunction sqrFunction = new SqrFunction(); // f(x) = x^2
 
     @Test
@@ -22,10 +21,6 @@ class RightSteppingDifferentialOperatorTest {
 
     @Test
     void deriveTest2() {
-        RightSteppingDifferentialOperator obj = new RightSteppingDifferentialOperator(-0.01);
-        MathFunction derivative = obj.derive(sqrFunction);
-
-        double x = 2.0;
-        assertEquals(2 * x, derivative.apply(x), EPS);
+        assertThrows(IllegalArgumentException.class, () -> new RightSteppingDifferentialOperator(-0.01));
     }
 }
