beab610457af1d8a4d032480b079c86d971cd796
Me-Di-A <melnicov730@gmail.com>
2025-10-14 14:33:21 +0300

    Update ReadWriteTaskExecutor.java

--- LBOOP3/untitled/src/main/java/ru/ssau/tk/samsa/LB2/concurrent/ReadWriteTaskExecutor.java
+++ LBOOP3/untitled/src/main/java/ru/ssau/tk/samsa/LB2/concurrent/ReadWriteTaskExecutor.java
@@ -2,18 +2,12 @@ package ru.ssau.tk.samsa.LB2.concurrent;
 
 import ru.ssau.tk.samsa.LB2.functions.ConstantFunction;
 import ru.ssau.tk.samsa.LB2.functions.TabulatedFunction;
-import ru.ssau.tk.samsa.LB2.functions.factory.LinkedListTabulatedFunctionFactory;
-
+import ru.ssau.tk.samsa.LB2.functions.LinkedListTabulatedFunction;
 
 public class ReadWriteTaskExecutor {
     public static void main(String[] args) {
         int count = 1000;
-        var factory = new LinkedListTabulatedFunctionFactory();
-
-        TabulatedFunction function = factory.create(
-                new ConstantFunction(-1),
-                1, 1000, count
-        );
+        TabulatedFunction function = new LinkedListTabulatedFunction(new ConstantFunction(-1), 1, 1000, count);
 
         Thread reader = new Thread(new ReadTask(function), "Reader");
         Thread writer = new Thread(new WriteTask(function, 0.5), "Writer");
