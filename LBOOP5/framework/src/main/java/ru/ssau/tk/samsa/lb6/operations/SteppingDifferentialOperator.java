4593df7fe2698e7ad07438630ee81761c11f7363
Me-Di-A <melnicov730@gmail.com>
2025-10-21 00:14:52 +0300

    Update SteppingDifferentialOperator.java

--- LBOOP5/framework/src/main/java/ru/ssau/tk/samsa/lb6/operations/SteppingDifferentialOperator.java
+++ LBOOP5/framework/src/main/java/ru/ssau/tk/samsa/lb6/operations/SteppingDifferentialOperator.java
@@ -1,4 +1,4 @@
-package ru.ssau.tk.samsa.LB2.lb6.jdbc.operations;
+package ru.ssau.tk.samsa.lb6.operations;
 
 public abstract class SteppingDifferentialOperator<T> implements DifferentialOperator<T> {
     protected double step;
@@ -18,3 +18,4 @@ public abstract class SteppingDifferentialOperator<T> implements DifferentialOpe
         this.step = step;
     }
 }
+
