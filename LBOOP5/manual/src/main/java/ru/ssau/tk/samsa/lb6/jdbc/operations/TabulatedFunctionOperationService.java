bfdba66b9f0077e73b9eb7a61a3924ac337a654f
Me-Di-A <melnicov730@gmail.com>
2025-10-20 23:16:56 +0300

    Update TabulatedFunctionOperationService.java

--- LBOOP5/manual/src/main/java/ru/ssau/tk/samsa/lb6/jdbc/operations/TabulatedFunctionOperationService.java
+++ LBOOP5/manual/src/main/java/ru/ssau/tk/samsa/lb6/jdbc/operations/TabulatedFunctionOperationService.java
@@ -1,8 +1,8 @@
-package ru.ssau.tk.samsa.LB2.operations;
+package ru.ssau.tk.samsa.LB2.lb6.jdbc.operations;
 
-import ru.ssau.tk.samsa.LB2.functions.*;
-import ru.ssau.tk.samsa.LB2.functions.factory.*;
-import ru.ssau.tk.samsa.LB2.exceptions.InconsistentFunctionsException;
+import ru.ssau.tk.samsa.LB2.lb6.jdbc.functions.*;
+import ru.ssau.tk.samsa.LB2.lb6.jdbc.functions.factory.*;
+import ru.ssau.tk.samsa.LB2.lb6.jdbc.exceptions.InconsistentFunctionsException;
 
 public class TabulatedFunctionOperationService {
     TabulatedFunctionFactory factory;
