403b53dec6daab4a5412146af31bc7f938492865
Me-Di-A <melnicov730@gmail.com>
2025-10-21 00:09:19 +0300

    Update DeBoorFunction.java

--- LBOOP5/framework/src/main/java/ru/ssau/tk/samsa/lb6/functions/DeBoorFunction.java
+++ LBOOP5/framework/src/main/java/ru/ssau/tk/samsa/lb6/functions/DeBoorFunction.java
@@ -1,4 +1,4 @@
-package ru.ssau.tk.samsa.LB2.lb6.jdbc.functions;
+package ru.ssau.tk.samsa.lb6.functions;
 
 public class DeBoorFunction implements MathFunction {
     private final double[] knots;
@@ -121,4 +121,5 @@ public class DeBoorFunction implements MathFunction {
     }
 
 }
+
 
