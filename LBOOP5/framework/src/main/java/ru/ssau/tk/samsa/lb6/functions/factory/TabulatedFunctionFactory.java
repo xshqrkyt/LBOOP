8ff432fe82cef73ff6f0c0468c5ee73d7a43ba4b
Me-Di-A <melnicov730@gmail.com>
2025-10-21 00:11:37 +0300

    Update TabulatedFunctionFactory.java

--- LBOOP5/framework/src/main/java/ru/ssau/tk/samsa/lb6/functions/factory/TabulatedFunctionFactory.java
+++ LBOOP5/framework/src/main/java/ru/ssau/tk/samsa/lb6/functions/factory/TabulatedFunctionFactory.java
@@ -1,7 +1,8 @@
-package ru.ssau.tk.samsa.LB2.lb6.jdbc.functions.factory;
+package ru.ssau.tk.samsa.lb6.functions.factory;
 
-import ru.ssau.tk.samsa.LB2.lb6.jdbc.functions.TabulatedFunction;
+import ru.ssau.tk.samsa.lb6.functions.TabulatedFunction;
 
 public interface TabulatedFunctionFactory {
     TabulatedFunction create(double[] xValues, double[] yValues);
 }
+
