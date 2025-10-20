0fc15a81a19019947de9fa07b2c5378f14eabd72
Me-Di-A <melnicov730@gmail.com>
2025-10-21 00:08:16 +0300

    Update MathFunction.java

--- LBOOP5/framework/src/main/java/ru/ssau/tk/samsa/lb6/functions/MathFunction.java
+++ LBOOP5/framework/src/main/java/ru/ssau/tk/samsa/lb6/functions/MathFunction.java
@@ -1,4 +1,4 @@
-package ru.ssau.tk.samsa.LB2.lb6.jdbc.functions;
+package ru.ssau.tk.samsa.lb6.functions;
 
 public interface MathFunction {
     double apply(double x);
@@ -7,3 +7,4 @@ public interface MathFunction {
         return new CompositeFunction(this, afterFunction);
     }
 }
+
