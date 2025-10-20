000d580ee1be123b7e728c39d904b6b10243d753
Me-Di-A <melnicov730@gmail.com>
2025-10-20 23:06:23 +0300

    Update ArrayIsNotSortedException.java

--- LBOOP5/manual/src/main/java/ru/ssau/tk/samsa/lb6/jdbc/exceptions/ArrayIsNotSortedException.java
+++ LBOOP5/manual/src/main/java/ru/ssau/tk/samsa/lb6/jdbc/exceptions/ArrayIsNotSortedException.java
@@ -1,4 +1,4 @@
-package ru.ssau.tk.samsa.LB2.exceptions;
+package ru.ssau.tk.samsa.LB2.lb6.jdbc.exceptions;
 
 public class ArrayIsNotSortedException extends RuntimeException {
     public ArrayIsNotSortedException() {}
