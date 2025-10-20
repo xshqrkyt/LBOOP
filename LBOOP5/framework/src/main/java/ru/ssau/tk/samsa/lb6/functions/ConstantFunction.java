6564400751ff62bd2d2290bb83fb1844a73a7284
Me-Di-A <melnicov730@gmail.com>
2025-10-21 00:09:32 +0300

    Update ConstantFunction.java

--- LBOOP5/framework/src/main/java/ru/ssau/tk/samsa/lb6/functions/ConstantFunction.java
+++ LBOOP5/framework/src/main/java/ru/ssau/tk/samsa/lb6/functions/ConstantFunction.java
@@ -1,4 +1,4 @@
-package ru.ssau.tk.samsa.LB2.lb6.jdbc.functions;
+package ru.ssau.tk.samsa.lb6.functions;
 
 public class ConstantFunction implements MathFunction {
     private final double CONST_NUM;
@@ -16,3 +16,4 @@ public class ConstantFunction implements MathFunction {
         return CONST_NUM;
     }
 }
+
