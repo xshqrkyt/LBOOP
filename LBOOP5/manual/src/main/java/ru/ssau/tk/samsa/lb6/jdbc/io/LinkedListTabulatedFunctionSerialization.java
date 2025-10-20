ec755d662d06146a940db93be09353b8f2e3e77c
Me-Di-A <melnicov730@gmail.com>
2025-10-20 23:14:39 +0300

    Update LinkedListTabulatedFunctionSerialization.java

--- LBOOP5/manual/src/main/java/ru/ssau/tk/samsa/lb6/jdbc/io/LinkedListTabulatedFunctionSerialization.java
+++ LBOOP5/manual/src/main/java/ru/ssau/tk/samsa/lb6/jdbc/io/LinkedListTabulatedFunctionSerialization.java
@@ -1,11 +1,11 @@
-package ru.ssau.tk.samsa.LB2.io;
+package ru.ssau.tk.samsa.LB2.lb6.jdbc.io;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import ru.ssau.tk.samsa.LB2.functions.*;
-import ru.ssau.tk.samsa.LB2.operations.TabulatedDifferentialOperator;
-import ru.ssau.tk.samsa.LB2.functions.factory.LinkedListTabulatedFunctionFactory;
+import ru.ssau.tk.samsa.LB2.lb6.jdbc.functions.*;
+import ru.ssau.tk.samsa.LB2.lb6.jdbc.operations.TabulatedDifferentialOperator;
+import ru.ssau.tk.samsa.LB2.lb6.jdbc.functions.factory.LinkedListTabulatedFunctionFactory;
 
 import java.io.*;
 
