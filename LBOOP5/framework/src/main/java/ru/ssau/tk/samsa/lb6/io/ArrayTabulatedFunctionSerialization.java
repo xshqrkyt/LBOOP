4e787490e707f51a28ec6b1f37b7f170fbb24dbc
Me-Di-A <melnicov730@gmail.com>
2025-10-21 00:11:58 +0300

    Update ArrayTabulatedFunctionSerialization.java

--- LBOOP5/framework/src/main/java/ru/ssau/tk/samsa/lb6/io/ArrayTabulatedFunctionSerialization.java
+++ LBOOP5/framework/src/main/java/ru/ssau/tk/samsa/lb6/io/ArrayTabulatedFunctionSerialization.java
@@ -1,12 +1,12 @@
-package ru.ssau.tk.samsa.LB2.lb6.jdbc.io;
+package ru.ssau.tk.samsa.lb6.io;
 
 import java.io.*;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import ru.ssau.tk.samsa.LB2.lb6.jdbc.functions.*;
-import ru.ssau.tk.samsa.LB2.lb6.jdbc.functions.factory.*;
-import ru.ssau.tk.samsa.LB2.lb6.jdbc.operations.TabulatedDifferentialOperator;
+import ru.ssau.tk.samsa.lb6.functions.*;
+import ru.ssau.tk.samsa.lb6.functions.factory.*;
+import ru.ssau.tk.samsa.lb6.operations.TabulatedDifferentialOperator;
 
 public class ArrayTabulatedFunctionSerialization {
     private static final Logger logger = LogManager.getLogger(TabulatedFunctionFileInputStream.class);
@@ -51,3 +51,4 @@ public class ArrayTabulatedFunctionSerialization {
         logger.info("Программа завершила свою работу.");
     }
 }
+
